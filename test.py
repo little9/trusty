bba506e1f75c286442991e466e5260e1b6d6687d
James Little <j.little@miami.edu>
2016-09-12 12:43:40 -0400

    Updating tests, keep subdirectories in the bag structure

--- test.py
+++ test.py
@@ -1,18 +1,31 @@
 import unittest
 from trusty import Trusty
+import bagit
+import shutil
 
 class TestTrustyFunctions(unittest.TestCase):
-   
-    def test_settings(self):
-        t = Trusty()
-        self.assertEqual(t.settings('settings.yaml')[':institution'][':institution'], 'example.edu')
-
-    def test_file_list(self):
-        t = Trusty()
-        file_list = t.file_list('.')
-        self.assertTrue('./settings.yaml' in file_list)
     
         
+    def test_settings(self):
+        t = Trusty('Title')
+        settings = t.load_settings('settings.yaml')
+        self.assertEqual(settings[':institution']['institution'], 'example.edu')
+
+    def test_file_list(self):
+        t = Trusty('Title')
+        file_list = t.file_list('.')
+        self.assertTrue('./settings.yaml' in file_list)
+        
+    def test_create_aptrust_bags(self):
+        t = Trusty('Title')
+        settings = t.load_settings('settings.yaml')
+        t.settings[':local_server']['storage_directory'] = './test_storage'
+        t.create_aptrust_bags('./test_data')
+        bag = bagit.Bag('./test_storage/example.edu.test_txt/')
+        self.assertTrue(bag.validate, True)
+        shutil.rmtree('./test_storage/example.edu.test_txt/')
+
+ 
 if __name__ == '__main__':
     unittest.main()
     
--- trusty.py
+++ trusty.py
@@ -23,25 +23,30 @@ class Trusty:
         file_list = []
         for root, sub_folders, files in os.walk(root_directory):
             for file in files:
-                file_list.append(os.path.join(root,file))
+                if self.settings[':local_server']['ignore'] not in file:
+                    file_list.append(os.path.join(root,file))
         return file_list
 
     def create_bag_dirs(self,file_list):
         """ Create the directories for the bags -- bagit.py only supports bag in place """
         dir_list = []
         for file in file_list:
+            sub_dirs = self.sub_dirs(file)
             file_name = file.rsplit('/', 1)[-1]
             dirname = self.dir_to_filename(file)
+    
             if not os.path.exists(dirname):
-                os.makedirs(dirname)
-                copyfile(file,dirname+"/"+file_name)
-                print(dirname+"/"+file_name)
-                dir_list.append(dirname)
+                os.makedirs(dirname+"/"+sub_dirs)
+                copyfile(file,dirname+"/"+sub_dirs+"/"+file_name)
+                dir_list.append(dirname+sub_dirs)
+                print dir_list
         return dir_list
 
     def create_bags(self,dir_list):
+        
         """ Actually create the bags and write the APTrust info file """
         for dir in dir_list:
+            dir = dir.split('/')[0]
             bag = bagit.make_bag(dir, {'Source-Organization': self.settings[':institution']['source_org'], 'Internal-Sender-Description' :'','Internal-Sender-Identifier':''})
             bag.save(manifests=True)
             self.write_aptrust_info(dir)
@@ -58,12 +63,17 @@ class Trusty:
         """ Create a valid APTrust bag name from a file path """
         return self.settings[':institution']['institution']+'.'+basename(filename).replace(" ","_").replace("/","_").replace(".","_")
 
-    def create_aptrust_bags(self):
-        dir_list = self.create_bag_dirs(self.file_list(sys.argv[1]))
-        print self.file_list(sys.argv[1])
-        print dir_list
+
+    def create_aptrust_bags(self,src):
+        dir_list = self.create_bag_dirs(self.file_list(src))
         self.create_bags(dir_list)
-        
+                                        
 
-
+    def sub_dirs(self, filepath):
+        """ Given an absolute filepath, return all the subdirectories """
+        dir_list = os.path.abspath(filepath).rsplit('/')
+        dir_list.pop()
+        dir_list.pop(0)
+        dir_list = "/".join(dir_list)
+        return "/" + dir_list
 
