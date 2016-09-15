3fe4c8e00f94a8850c309fce4a4560b81ba2dbff
James Little <j.little@miami.edu>
2016-09-15 14:22:08 -0400

    More changes from APTrust testing

--- test.py
+++ test.py
@@ -1,21 +1,14 @@
+ # -*- coding: utf-8 -*-
 import unittest
 from trusty import Trusty
 import bagit
 import shutil
 
 class TestTrustyFunctions(unittest.TestCase):
-    def test_settings(self):
-        t = Trusty({'title':'Title','description':'A description','access':'Restricted'})
-        self.assertEqual(t.settings[':institution']['institution'], 'example.edu')
-
-    def test_file_list(self):
-        t = Trusty({'title':'Title','description':'A description','access':'Restricted'})
-        file_list = t.file_list('.')
-        self.assertTrue('./settings.yaml' in file_list)
-        
+           
     def test_create_aptrust_bags(self):
-        t = Trusty({'title':'Title','description':'A description','access':'Restricted'})
-       
+        t = Trusty({'title':'Title','description':'A description','access':'Restricted','subdir':''})
+        t.settings[':institution']['institution'] = 'example.edu'
         t.settings[':local_server']['storage_directory'] = './test_storage'
         t.create_aptrust_bags('./test_data')
         bag = bagit.Bag('./test_storage/example.edu.test_txt/')
--- trusty.py
+++ trusty.py
@@ -1,19 +1,22 @@
+ # -*- coding: utf-8 -*-
 import os
 from os.path import basename
 import sys
 import yaml
 import urllib
-from shutil import copyfile, move
+from shutil import copyfile, move, copytree
 import bagit
+import logging
 
 class Trusty(object):
-    def __init__(self, aptrust_info):
+    def __init__(self, collection_meta):
         self.settings = self.load_settings('./settings.yaml')
-        self.title = aptrust_info['title']
-        self.access = aptrust_info['access']
-        self.description = aptrust_info['description']
-    
-    
+        self.title = collection_meta['title']
+        self.access = collection_meta['access']
+        self.description = collection_meta['description']
+        self.remove_sub_dir = collection_meta['subdir']
+        self.original_root = self.settings[':local_server']['storage_root']
+        self.logging = logging.getLogger()
         
     def load_settings(self,settings_file):
         """ Load the settings from the settings.yaml file """
@@ -36,6 +39,7 @@ class Trusty(object):
         for file in file_list:
             sub_dirs = self.sub_dirs(file)
             file_name = file.rsplit('/', 1)[-1]
+           
             dirname = self.dir_to_filename(file)
     
             if not os.path.exists(dirname):
@@ -63,20 +67,39 @@ class Trusty(object):
             
     def dir_to_filename(self,filename):
         """ Create a valid APTrust bag name from a file path """
-        return self.settings[':institution']['institution']+'.'+basename(filename).replace(" ","_").replace("/","_").replace(".","_")
+        if self.remove_sub_dir:
+            new_filename = filename.replace(self.remove_sub_dir,'')
+            dir_filename = self.settings[':institution']['institution']+'.'+new_filename[2:].replace(" ","_").replace("/","_").replace(".","_")
+        else:
+            
+            dir_filename = self.settings[':institution']['institution']+'.'+basename(filename).replace(" ","_").replace("/","_").replace(".","_")
+        return dir_filename 
 
 
     def create_aptrust_bags(self,src):
         """ Create APTrust bags for all files in a directory """
         dir_list = self.create_bag_dirs(self.file_list(src))
         self.create_bags(dir_list)
-                                        
 
+    def create_aptrust_metadata_bag(self, src,storage_dir):
+        no_root_dir = src.replace(storage_dir,'')
+        dirname ='miami.edu.'+no_root_dir
+        copytree(src, dirname)
+        bag = bagit.make_bag(dirname, {'Source-Organization': self.settings[':institution']['source_org'], 'Internal-Sender-Description' :'','Internal-Sender-Identifier':''})
+        bag.save(manifests=True)
+        self.write_aptrust_info(dirname)
+        move(dirname, self.settings[':local_server']['storage_directory'])
+       
     def sub_dirs(self, filepath):
-        """ Given an absolute filepath, return all the subdirectories """
+        """ Given an absolute filepath, return all the subdirectories, if you don't want some subdirs, you can pass a parameters when creating an instance of the class and it will be removed """
         dir_list = os.path.abspath(filepath).rsplit('/')
         dir_list.pop()
         dir_list.pop(0)
-        dir_list = "/".join(dir_list)
-        return "/" + dir_list
+        join_dir_list = "/".join(dir_list)
+        if self.remove_sub_dir:
+            new_dir_list = join_dir_list.replace(self.remove_sub_dir,'')
+        else:
+            new_dir_list = join_dir_list
+            
+        return "/" + new_dir_list
 
